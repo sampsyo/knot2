3b863d0a107b10bb6ce705c709d90be753906f3d
Adrian Sampson <adrian@radbox.org>
2025-12-31 14:49:44 -0500

    Try scoped threads
    What I was trying to do is impossible, because of the leakpocalypse. I
    wanted a way for the `WorkPool` to borrow stuff from the main thread,
    which it then "relinquishes" when dropped, but that's not possible. This
    post finally explained it in a way I can understand:
    https://without.boats/blog/the-scoped-task-trilemma/

--- src/parallel.rs
+++ src/parallel.rs
@@ -1,41 +1,36 @@
 use crossbeam_channel::{Sender, bounded};
 use std::thread;
 
-struct WorkPool<T: Send + 'static> {
+pub struct WorkPool<T: Send + 'static> {
     tx: Sender<T>,
-    threads: Vec<thread::JoinHandle<()>>,
 }
 
-impl<T: Send + 'static> WorkPool<T> {
-    pub fn new<F>(thread_count: usize, chan_size: usize, work_fn: F) -> Self
-    where
-        F: Fn(T) + Send + Clone + 'static,
-    {
+pub fn run_pool<T, W, B>(thread_count: usize, chan_size: usize, work_fn: W, body_fn: B)
+where
+    T: Send + 'static,
+    W: Fn(T) + Send + Clone + 'static,
+    B: Fn(WorkPool<T>),
+{
+    thread::scope(|s| {
         let (tx, rx) = bounded(chan_size);
 
-        let mut threads = Vec::with_capacity(thread_count);
         for _ in 0..thread_count {
             let thread_rx = rx.clone();
             let thread_work = work_fn.clone();
-            threads.push(thread::spawn(move || {
+            s.spawn(move || {
                 while let Ok(val) = thread_rx.recv() {
                     thread_work(val);
                 }
-            }));
+            });
         }
 
-        Self { tx, threads }
-    }
+        body_fn(WorkPool { tx });
+    });
+}
 
+impl<T: Send + 'static> WorkPool<T> {
     pub fn send(&self, value: T) {
         self.tx.send(value).unwrap();
-    }
-
-    pub fn join(self) {
-        drop(self.tx);
-        for t in self.threads {
-            t.join().unwrap();
-        }
     }
 }
 
@@ -60,17 +55,21 @@ mod tests {
         let res_lock = Arc::new(Mutex::new(results));
         let res_lock2 = res_lock.clone();
 
-        let pool = WorkPool::new(8, 32, move |i| {
-            let p = is_prime(i);
-            let mut r = res_lock.lock().unwrap();
-            r.insert(i, p);
-        });
-
-        pool.send(5);
-        pool.send(10);
-        pool.send(15);
-        pool.send(19);
-        pool.join();
+        run_pool(
+            8,
+            32,
+            move |i| {
+                let p = is_prime(i);
+                let mut r = res_lock.lock().unwrap();
+                r.insert(i, p);
+            },
+            |pool| {
+                pool.send(5);
+                pool.send(10);
+                pool.send(15);
+                pool.send(19);
+            },
+        );
 
         let results = res_lock2.lock().unwrap();
         let mut res_pairs: Vec<_> = results.iter().collect();
